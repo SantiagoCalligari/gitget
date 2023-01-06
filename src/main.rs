9e81d27918c5f299dc7b13ee8907e979a3dab689
Santiago Calligari <santiago@calligari.ar>
2023-01-06 01:28:10 -0300

    I formated the string that came from the request to json.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 use std::env;
 
-#[tokio::main]
-async fn main() {
-    let args: Vec<String> = env::args().collect();
-    let git_user:&String = &args[1];
+macro_rules! unwrap_or_return {
+    ( $e:expr ) => {
+        match $e {
+            Ok(x) => x,
+            Err(_) => String::from(""),
+        }
+    }
+}
+
+fn make_url(git_user: &String) -> String {
     let mut url:String = String::from("https://api.github.com/users/");
     url.push_str(git_user);
     url.push_str("/repos");
-    
-    println!("estoy pidiendo esto: {}", url);
-    let repos = get_github_repos(url).await;
-    println!("{:#?}", repos);
+    url
+}
+async fn json_results(url:String) -> serde_json::Value {
+    let results:&str = &unwrap_or_return!(get_github_repos(url).await)[..];
+    let json_results: serde_json::Value = 
+        serde_json::from_str(results).expect("JSON was not well-formated");
+    json_results
+}
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let url = make_url(&args[1]);
+    println!("{:#?}", json_results(url).await);
 }
 
 async fn get_github_repos(url: String) -> Result<String, Box<dyn std::error::Error>> {
